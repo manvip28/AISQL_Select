96d874750709c60acd37b09ac92f036c8a940376
manvip28 <159559667+manvip28@users.noreply.github.com>
2025-02-23 22:25:04 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import QueryForm from "./QueryForm"; // Import the QueryForm component
+
+function App() {
+  return (
+    <div className="App">
+      <QueryForm /> {/* Render the Query Form */}
+    </div>
+  );
+}
+
+export default App;
